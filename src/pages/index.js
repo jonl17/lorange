2f683dbfb8f430722f8f4f40d087faf6e9a7301c
Nonni Linux <jglorange@gmail>
2019-10-16 10:06:52 +0000

    sub site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import styled from "styled-components"
+
+const Text = styled.p`
+  width: 100%;
+  text-align: center;
+  font-size: 50px;
+`
 
 export default () => (
-  <p className="bold">Jón Gabríel Lorange - jglorange@gmail.com</p>
+  <Text className="bold">
+    Jón Gabríel Lorange - Web developer - jglorange@gmail.com
+  </Text>
 )
